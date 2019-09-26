2dad5e237133ba1104cac5c7d065ea30f5b6b5e4
anetczuk <anetczuk@o2.pl>
2019-09-26 20:04:40 +0200

    Cleanup

--- src/mem_performance/plot_all.sh
+++ src/mem_performance/plot_all.sh
@@ -13,22 +13,6 @@ curr_script=$(basename $BASH_SOURCE)
 
 argv=$@
 
-maxmem=""
-while test $# != 0; do
-    case "$1" in
-	    --maxmem|-maxmem) 	maxmem="$2"
-	    					shift
-	    					;;
-	    *)  ;;
-    esac
-    shift
-done
-
-if [ "$maxmem" == "" ]; then
-	echo "'--maxmem' argument required, exiting"
-	exit 1
-fi
-
 
 for filename in plot_*.sh; do
 	## executing script
