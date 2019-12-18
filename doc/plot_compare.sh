53140d39617fdd9973a3eea6ad864d300295a85f
anetczuk <anetczuk@o2.pl>
2019-12-18 21:13:08 +0100

    Add comparison plots

--- doc/plot_compare.sh
+++ doc/plot_compare.sh
@@ -22,65 +22,103 @@ merge_curves() {
     local -n curve_labels=$2
     local plot_title=$3
         
-    if [ -z "$plot_data_files" ]; then
+    if [ -z "$curves_data" ]; then
         echo "No data to plot"
-    else
-        local plot_png="$4"
-        echo "Plotting $plot_title"
-        gnuplot -p -e '
-                        set title "'"$plot_title"'"; 
-                        curves_num   = "'"${#curves_data[@]}"'";
-                        curve_labels = "'"${curve_labels[*]}"'";
-                        filenames    = "'"${curves_data[*]}"'";
-                        output_png   = "'"$plot_png"'"
-                      ' plot_config_multi.gnu
+        return ;
     fi
+    
+    local plot_png="$4"
+
+    local plot_string=""      
+    plot_string="plot"
+    for i in "${!curves_data[@]}"; do
+        local in_file=${curves_data[$i]}
+        if [ ! -f "$in_file" ]; then
+            echo "Data file not found: $in_file"
+            return ;
+        fi 
+        plot_string="${plot_string} \"$in_file\" using 1:2 title \"${curve_labels[$i]}\" with lines lw 3,"
+    done
+
+    ## echo -e "plot subcommand:\n${plot_string}"
+    
+    gnuplot -p -e '
+                    call "plot_config_head.gnu";
+                    set title "'"${plot_title}"'"; 
+                    output_png = "'"${plot_png}"'";
+                    '"${plot_string}"'; 
+                    call "plot_config_foot.gnu";
+                  '
 }
 
 
 ## $1 -- device
 ## $2 -- experiment
+## $3 -- title
 single_plot() {
-    local plot_data_files=("$SCRIPT_DIR/$1/gcc/$2_data_plot.txt" "$SCRIPT_DIR/$1/clang/$2_data_plot.txt")
-    local compiler_labels=("gcc" "clang")
-    local plot_png="$1/$2_comparison.png"
-    merge_curves plot_data_files compiler_labels "$3 compilers comparison" "$plot_png"
+    local data_file="$2.txt"
+    local plot_data_files=( "$SCRIPT_DIR/measurements/$1/gcc/$data_file" 
+                            "$SCRIPT_DIR/measurements/$1/gcc_unroll/$data_file" 
+                            "$SCRIPT_DIR/measurements/$1/clang/$data_file" 
+                            "$SCRIPT_DIR/measurements/$1/clang_unroll/$data_file"
+                            )
+    local compiler_labels=("gcc" "gcc unroll" "clang" "clang unroll")
+    local plot_png="measurements/$1/$2_comparison.png"
+    echo "$1: plotting $3"
+    merge_curves plot_data_files compiler_labels "$3" "$plot_png"
+}
+
+
+## $1 -- device
+## $2 -- title
+compiler_single_plot() {
+    single_plot "$1" array_st_data_plot "Comparison of single-threaded raw array access times"
+    single_plot "$1" vector_st_data_plot "Comparison of single-threaded std::vector access times"
+    single_plot "$1" cllist_st_data_plot "Comparison of single-threaded linked list access times"
+    
+    single_plot "$1" vector_mt_data_plot_average "Comparison of multi-threaded std::vector access times"
+    single_plot "$1" cllist_mt_data_plot_average "Comparison of multi-threaded linked list access times"
+    
+    single_plot "$1" vector_mp_data_plot_average "Comparison of multi-processed std::vector access times"
+    single_plot "$1" cllist_mp_data_plot_average "Comparison of multi-processed linked list access times"
 }
 
 
 
 ## comparison of compilers on single device
-single_plot i7 vector_st "Intel Core i7"
-single_plot rpi3 vector_st "Raspberry Pi 3"
+compiler_single_plot i7
+compiler_single_plot rpi3
+compiler_single_plot i7_vbox_1
+compiler_single_plot i7_vbox_2
 
-
-## comparison of host vs virutal box 
-plot_data_files=("$SCRIPT_DIR/i7/gcc/vector_st_data_plot.txt" "$SCRIPT_DIR/i7_vbox_1/gcc/vector_st_data_plot.txt")
-plot_labels=("host" "vbox")
-merge_curves plot_data_files plot_labels "Comparison of host and Virtual Box on std::vector" "i7_vbox_1/host_vector_comparison.png"
-
-plot_data_files=("$SCRIPT_DIR/i7/gcc/cllist_st_data_plot.txt" "$SCRIPT_DIR/i7_vbox_1/gcc/cllist_st_data_plot.txt")
-plot_labels=("host" "vbox")
-merge_curves plot_data_files plot_labels "Comparison of host and Virtual Box on linked list" "i7_vbox_1/host_cllist_comparison.png"
-
-
-## comparison of gcc versions on vbox
-plot_data_files=("$SCRIPT_DIR/i7_vbox_1/gcc/vector_st_data_plot.txt" "$SCRIPT_DIR/i7_vbox_2/gcc/vector_st_data_plot.txt")
-plot_labels=("vbox 1" "vbox 2")
-merge_curves plot_data_files plot_labels "Comparison of clang versions on std::vector" "i7_vbox_2/gcc_vector_comparison.png"
-
-plot_data_files=("$SCRIPT_DIR/i7_vbox_1/gcc/cllist_st_data_plot.txt" "$SCRIPT_DIR/i7_vbox_2/gcc/cllist_st_data_plot.txt")
-plot_labels=("vbox 1" "vbox 2")
-merge_curves plot_data_files plot_labels "Comparison of GCC versions on linked list" "i7_vbox_2/gcc_cllist_comparison.png"
-
-
-## comparison of clang versions on vbox
-plot_data_files=("$SCRIPT_DIR/i7_vbox_1/clang/vector_st_data_plot.txt" "$SCRIPT_DIR/i7_vbox_2/clang/vector_st_data_plot.txt")
-plot_labels=("7.4.0" "9.2.1")
-merge_curves plot_data_files plot_labels "Comparison of clang versions on std::vector" "i7_vbox_2/clang_vector_comparison.png"
-
-plot_data_files=("$SCRIPT_DIR/i7_vbox_1/clang/cllist_st_data_plot.txt" "$SCRIPT_DIR/i7_vbox_2/clang/cllist_st_data_plot.txt")
-plot_labels=("6.0.0" "9.0.0")
-merge_curves plot_data_files plot_labels "Comparison of GCC versions on linked list" "i7_vbox_2/clang_cllist_comparison.png"
-
+aaa() {
+    ## comparison of host vs virtual box 
+    plot_data_files=("$SCRIPT_DIR/i7/gcc/vector_st_data_plot.txt" "$SCRIPT_DIR/i7_vbox_1/gcc/vector_st_data_plot.txt")
+    plot_labels=("host" "vbox")
+    merge_curves plot_data_files plot_labels "Comparison of host and Virtual Box on std::vector" "i7_vbox_1/host_vector_comparison.png"
+    
+    plot_data_files=("$SCRIPT_DIR/i7/gcc/cllist_st_data_plot.txt" "$SCRIPT_DIR/i7_vbox_1/gcc/cllist_st_data_plot.txt")
+    plot_labels=("host" "vbox")
+    merge_curves plot_data_files plot_labels "Comparison of host and Virtual Box on linked list" "i7_vbox_1/host_cllist_comparison.png"
+    
+    
+    ## comparison of gcc versions on vbox
+    plot_data_files=("$SCRIPT_DIR/i7_vbox_1/gcc/vector_st_data_plot.txt" "$SCRIPT_DIR/i7_vbox_2/gcc/vector_st_data_plot.txt")
+    plot_labels=("vbox 1" "vbox 2")
+    merge_curves plot_data_files plot_labels "Comparison of clang versions on std::vector" "i7_vbox_2/gcc_vector_comparison.png"
+    
+    plot_data_files=("$SCRIPT_DIR/i7_vbox_1/gcc/cllist_st_data_plot.txt" "$SCRIPT_DIR/i7_vbox_2/gcc/cllist_st_data_plot.txt")
+    plot_labels=("vbox 1" "vbox 2")
+    merge_curves plot_data_files plot_labels "Comparison of GCC versions on linked list" "i7_vbox_2/gcc_cllist_comparison.png"
+    
+    
+    ## comparison of clang versions on vbox
+    plot_data_files=("$SCRIPT_DIR/i7_vbox_1/clang/vector_st_data_plot.txt" "$SCRIPT_DIR/i7_vbox_2/clang/vector_st_data_plot.txt")
+    plot_labels=("7.4.0" "9.2.1")
+    merge_curves plot_data_files plot_labels "Comparison of clang versions on std::vector" "i7_vbox_2/clang_vector_comparison.png"
+    
+    plot_data_files=("$SCRIPT_DIR/i7_vbox_1/clang/cllist_st_data_plot.txt" "$SCRIPT_DIR/i7_vbox_2/clang/cllist_st_data_plot.txt")
+    plot_labels=("6.0.0" "9.0.0")
+    merge_curves plot_data_files plot_labels "Comparison of GCC versions on linked list" "i7_vbox_2/clang_cllist_comparison.png"
+}
 
